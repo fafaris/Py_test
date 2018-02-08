639f6fd30e15a6fc5589ffd122fbe1e5eccf7917
fafaris <36257444+fafaris@users.noreply.github.com>
2018-02-08 14:35:14 +0700

    test

--- json_1.py
+++ json_1.py
@@ -9,9 +9,12 @@ port = int(os.getenv("PORT"))
 @route('/',mehtod="GET")
 def input():
     now = datetime.datetime.now()
-    a = int(request.query['a'])
-    b = int(request.query['b'])
-    c = a + b
-    return str(c),"   ",str(now)
+    try:
+        a = int(request.query['a'])
+        b = int(request.query['b'])
+        c = a + b
+        return str(c),"   ",str(now)
+    except:
+        return ("กรุณาระบุตัวเลข")
  #if__name__ == '__name__':
 run(host='0.0.0.0',port=port)
