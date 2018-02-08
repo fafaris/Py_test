b76006c2eed657a32e0fb916dafc94d57267dd19
fafaris <36257444+fafaris@users.noreply.github.com>
2018-02-08 17:00:27 +0700

    สร้าง method post และ get
    สร้าง javascript ajax และ post

--- json_1.py
+++ json_1.py
@@ -6,15 +6,18 @@ from bottle import route,run , post,request
 
 port = int(os.getenv("PORT"))
 
-@route('/',mehtod="GET")
+@route('/',method="POST")
 def input():
     now = datetime.datetime.now()
     try:
-        a = int(request.query['a'])
-        b = int(request.query['b'])
+        a = int(request.POST['a'])
+        b = int(request.POST['b'])
         c = a + b
-        return str(c),"   ",str(now)
+        return {"message":str(c)+"   "+str(now)}
     except:
-        return ("กรุณาระบุตัวเลข")
+        return ("error")
+@route('/',method="GET")
+def input():
+    return '<html><head><script>function loadDoc() {var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {var obj = JSON.parse(this.responseText);document.getElementById("demo").innerHTML = obj.message;}};xhttp.open("POST", "/", true);var formData = new FormData();formData.append("a",document.getElementById("a").value);formData.append("b",document.getElementById("b").value);xhttp.send(formData);}</script></head><body><form action = "/" method = "POST"><input type = "text" name="a" id = "a"><input type = "text" name="b" id = "b"><input type = "button" value="send" onclick = "loadDoc()"></form><div id = "demo"></div></body></html>'
  #if__name__ == '__name__':
 run(host='0.0.0.0',port=port)
